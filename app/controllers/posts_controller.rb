b7c54fff72c3288a7eecbcfc5f4884d28d595d82
mutsukitokunaga <mutsukitokunaga@outlook.jp>
2020-12-07 06:45:15 +0000

    編集機能追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,17 +8,27 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def show
     @post = Post.find(params[:id])
   end
 
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to posts_path, notice: "つぶやきを編集しました！"
+    else
+      render :edit
+    end
+  end
+
   def destroy
   end
 
   def create
-    @post = Post.create(content: params[:post][:content])
+    @post = Post.create(post_params)
     if @post.save
       redirect_to new_post_path
       # 一覧画面へ遷移して"ブログを作成しました！"とメッセージを表示します。
@@ -27,6 +37,10 @@ class PostsController < ApplicationController
       # newのアクションにうつる
       render :new
     end
+  end
+
+  def post_params
+    params.require(:post).permit(:content)
   end
 
 end
